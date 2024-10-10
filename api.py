3aeaba34f3d1ab8b579bdb43cbdf75318ede1652
jivjen <jival.jisil@yahoo.co.in>
2024-10-11 03:32:06 +0530

    workers

--- api.py
+++ api.py
@@ -84,4 +84,4 @@ async def get_job_result(job_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=4)
