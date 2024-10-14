4452821db5ec592c39f7f1d8fb12f549c2a411d9
jivjen <jival.jisil@yahoo.co.in>
2024-10-14 09:40:41 +0530

    aooo

--- logging_config.py
+++ logging_config.py
@@ -5,30 +5,30 @@ from datetime import datetime
 def setup_logger(job_id):
     log_dir = "logs"
     os.makedirs(log_dir, exist_ok=True)
-    
+
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     log_file = os.path.join(log_dir, f"{job_id}_{timestamp}.log")
-    
+
     # Get the root logger
     logger = logging.getLogger()
-    
+
     # Remove all existing handlers
     for handler in logger.handlers[:]:
         logger.removeHandler(handler)
-    
+
     logger.setLevel(logging.DEBUG)
-    
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    
+
     file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
-    
+
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
-    
+
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-    
+
     return logger
